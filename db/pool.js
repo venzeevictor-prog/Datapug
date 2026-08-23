26bb4c69f06ba74e105b9c67457f8e4f047a06bc
venzeevictor-prog <venzeevictor@gmail.com>
2026-08-23 02:11:51 +0100

    Refactor SSL configuration for database pool

--- db/pool.js
+++ db/pool.js
@@ -10,7 +10,9 @@ const isLocalDb = /localhost|127\.0\.0\.1/.test(process.env.DATABASE_URL || '');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isLocalDb ? false : { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('error', (err) => {
